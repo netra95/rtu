d69574edcac71575b2ebd9b6f0a362078c3664b4
Netra Heng <netra.heng@edu.devinci.fr>
2021-01-25 06:24:41 +0100

    Project commit on github

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
+import TodoList from './components/TodoList'
+import Header from './components/layout/Header';
+import uuid from 'uuid';
+import axios from 'axios';
+
+class App extends Component {
+
+  state = {
+    todoTitles: []
+  }
+
+  componentDidMount(){
+    this.getTitles();
+  }
+
+  getTitles() {
+    axios.get('/GetTitles')
+    .then(res => this.setState({ todoTitles: res.data.data}));
+  }
+
+  addTodoList= (title, todo) => {
+    axios.get(`/AddTodo?todo='${todo}'&title='${title}'`).then(res => console.log(res)).then(() => this.getTitles());
+  }
+
+  render() {
+    var appBody =
+        this.state.todoTitles.map((todos) => (
+          <Todos key={uuid.v4()} todoTitles = {todos} getTitles = {this.getTitles.bind(this)} />
+        ));
+    return (
+      <div className="body">
+        <Header />
+        <div className="myContainer">
+        {appBody}
+        <TodoList addTodoList={this.addTodoList}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
